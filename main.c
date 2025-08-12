8c48c1d71a843d776bdc2b23f6934b8a846b6261
SiAyoub <ayoubmarzougui03@gmail.com>
2025-08-12 21:47:05 +0100

    Add INFO in socket creation

--- main.c
+++ main.c
@@ -5,6 +5,13 @@ int main()
 {
 int server_fd;
 //Creating socket
+//https://www.geeksforgeeks.org/c/socket-programming-cc/
+//Socket()function to create a socket
+//the server_fd is an integer socket discriptor
+//socket(domain,type,protocole)
+//domain: integer, specifies communication domain. 
+//type: communication type.
+//protocol: Protocol value for Internet Protocol(IP), which is 0.
 server_fd=socket(AF_INET,SOCK_STREAM,0);
 	if (server_fd==0){
 	perror("socket failed");
